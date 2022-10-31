a2fb0c7527e23b8898ca7bcd2be677607303e7f2
Richie Junior <88040624+g1tgudd@users.noreply.github.com>
2022-11-01 04:57:16 +0700

    Nomor 3

---
+++ src/nomor3.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+struct Color {
+
+    red: (String, String, String),
+    green: (String, String, String),
+    blue: (String, String, String),
+    purple: (String, String, String),
+    orange: (String, String, String),
+    violet: (String, String, String),
+    maroon: (String, String, String),
+    grey: (String, String, String),
+    white: (String, String, String),
+    brown: (String, String, String)
+
+}
+
+fn return_format_value(color: &str) {
+
+}
+
+fn main() {
+
+}
