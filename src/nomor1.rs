ac45cd810c3cd2bf0b7de837a260f0d199bad943
Richie Junior <88040624+g1tgudd@users.noreply.github.com>
2022-11-01 04:57:06 +0700

    Nomor 1

--- src/nomor1.rs
+++ src/nomor1.rs
@@ -1,7 +1,12 @@
-fn reverse_string(inptstr: &str) {
+fn reverse_string(inptstr: &str) -> String {
     
+    inptstr.trim().chars().rev().collect::<String>()
 }
 
 fn main() {
     
+    let inptstr = "Test";
+    let result = reverse_string(&inptstr);
+    
+    println!("Reversed string = {:?}", result);
 }
