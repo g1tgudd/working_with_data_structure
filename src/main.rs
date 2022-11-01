a37c4f567cc88d5aa03bee3e64cb17b046bda3b2
Richie Junior <88040624+g1tgudd@users.noreply.github.com>
2022-11-01 07:31:01 +0700

    Main files

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,27 @@
-fn main() {}
+fn find_prime(input: &i32) -> bool {
+
+    if *input <= 1 {
+        return false;
+    }
+    for n in 2..*input {
+        if *input % n == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn convert_bool(input: &[i32]) -> Vec<bool> {
+
+    return input.iter().map(find_prime).collect(); 
+}
+
+fn main() {
+
+    let input: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let result = convert_bool(&input);
+
+    for (bool, value) in input.iter().enumerate() {
+        println!("{} = {:?}", value, result.get(bool).unwrap());
+    }
+}
